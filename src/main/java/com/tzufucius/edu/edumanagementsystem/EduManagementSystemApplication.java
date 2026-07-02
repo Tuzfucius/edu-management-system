3c486105dedf024cb92be9f2b9ac681ecebb97e3
Tzufucius <952105672@qq.com>
2026-07-02 22:47:54 +0800

    init

---
+++ src/main/java/com/tzufucius/edu/edumanagementsystem/EduManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tzufucius.edu.edumanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tzufucius/edu/edumanagementsystem/EduManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzufucius.edu.edumanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
